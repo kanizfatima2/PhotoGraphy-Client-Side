638c0751db879fbaefc0846738138588dd8da0e6
kanizfatima2 <kaniz686928@gmail.com>
2022-11-11 23:07:03 +0600

    s

---
+++ src/components/AddService/AddService.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AllService from '../Home Page/AllService';
+
+
+const AddService = () => {
+    return (
+        <div>
+            <AllService></AllService>
+        </div>
+    );
+};
+
+export default AddService;
